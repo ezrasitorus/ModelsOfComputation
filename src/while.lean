e194cb9605857fb225a542c52c2ee3125d9238ea
Ezra Sitorus <es1419@ic.ac.uk>
2020-11-03 17:03:11 +0000

    Added comments

--- src/while.lean
+++ src/while.lean
@@ -19,13 +19,13 @@ inductive w_com : Type
     | if_then_else : w_bool → w_com → w_com
     | seq_comp : w_com → w_com → w_com
     | skip : w_com
-    | while_do : w_bool → w_com
+    | while_do : w_bool → w_com → w_com
 
 open w_exp
 
 inductive b_eval : w_exp → ℕ → Prop
     | bnum : ∀ n : ℕ, b_eval (nat n) n
-    | badd : ∀ E₁ E₂ : w_exp, ∀ n₁ n₂: ℕ, b_eval E₁ n₁ → b_eval E₂ n₂ → b_eval (add E₁ E₂) (n₁ + n₂)
+    | badd : ∀ E₁ E₂ : w_exp, ∀ n₁ n₂: ℕ, b_eval E₁ n₁ → b_eval E₂ n₂ → b_eval (add E₁ E₂) (n₂ + n₁)
 
 open b_eval
 
@@ -64,26 +64,40 @@ end
 
 theorem b_step_determinacy : ∀ E : w_exp, ∀ n₁ n₂ : ℕ, b_eval E n₁ → b_eval E n₂ → n₁ = n₂ := 
 begin
-    intro E,
-    induction E with n E1 E2 ih1 ih2,
+    -- This is a proof that big step evaluation is deterministic
+    intro E, -- Introduces a term E : w_exp
+    induction E with n E1 E2 ih1 ih2, -- Doing induction on E, the with part is naming the term used later
     {
-        intros n1 n2 h1 h2,
-        cases h1,
-        cases h2,
-        refl,
+        -- This is the base case. E = n.
+        intros n1 n2 h1 h2, -- Introduces n1 n2 : ℕ and assumes b_eval n n1 and b_eval n n2. 
+        -- These proofs are a type, and h1 and h2 are terms of this type
+        cases h1, -- Doing a case analysis on h1, this changes n1 to n
+        cases h2, -- Doing a case analysis on h2, this changes n2 to n
+        refl, -- The goal is now n = n, which can be proved using the refl tactic
     },
     {
-        intros m n h1 h2,
-        cases h1,
+        -- This is the inductive case. We have E1 and E2 the proposition is true for them.
+        -- We want to prove for E1 + E2
+        intros m n h1 h2, -- Introduces n1 n2 : ℕ h1 h2 as before
+        -- Case analysis on h1. This is like saying h1 can only be obtained using badd, so what
+        -- other proofs can we infer. This is that m must be of the form a + b, for some naturals a b.
+        -- We also get a proof of b_eval E1 a and b_eval E2 b. The same is done for h2.
+        cases h1, 
         cases h2,
+
+        -- We specialise our inductive hypotheses with the proofs we obtained above
         specialize ih1 h1_n₁ h2_n₁ h1_a h2_a,
         specialize ih2 h1_n₂ h2_n₂ h1_a_1 h2_a_1,
-        simp, linarith,
+
+        simp, -- simp simplifies the goal using definitions and other known stuff (maths). 
+        -- Now the goal is about proving that adding some numbers give the same result
+        linarith, -- linarith is used to prove stuff in arithmetic
     }
 end
 
 theorem b_step_totality : ∀ E : w_exp, ∃ n₁ : ℕ, b_eval E n₁ := 
 begin
+    -- This is a proof that big step evaluation is total
     intro E,
     induction E with m E1 E2 ih1 ih2,
     {
@@ -94,7 +108,7 @@ begin
         rcases ih1 with ⟨m, hm⟩,
         rcases ih2 with ⟨n, hn⟩,
         use m + n,
-        exact badd E1 E2 m n hm hn,
+        exact badd E1 E2  hm hn,
     }
 end
 
@@ -159,6 +173,7 @@ def normal_form (E : w_exp) : Prop := ∀ E' : w_exp, ¬ s_eval E E'
 
 theorem s_step_determinacy : ∀ E E1 E2 : w_exp, s_eval E E1 → s_eval E E2 → E1 = E2 :=
 begin
+    -- This is a proof that small step evaluation is deterministic
     intro E,
     induction E with m E1 E2 ih1 ih2,
     {
@@ -176,7 +191,7 @@ begin
                 -- This can be easily dealt with using the inductive hypothesis on E1
                 have help : h1_E1' = h2_E1',
                     exact ih1 _ _ h1_a h2_a,
-                simp [help],
+                rw help,
             },
             -- The other cases are when E2' are n + E or n
             -- This means E1 is n and cannot be evaluated any further
@@ -207,7 +222,7 @@ begin
                     {
                         -- We use the inductive hypothesis to prove the s_right case
                         specialize ih2 _ _ h1_a h2_a,
-                        simp [ih2]
+                        rw ih2,
                     }
                 },
                 {
